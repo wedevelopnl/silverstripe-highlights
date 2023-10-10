ff13c2c41295994bde4e1cf8abd182a54e5e25af
Dennis Prins <dennis@wedevelop.nl>
2023-10-10 11:27:36 +0200

    Initial setupo

--- src/Extension/DBStringExtension.php
+++ src/Extension/DBStringExtension.php
@@ -15,14 +15,18 @@ class DBStringExtension extends Extension
 
     private static string $shortcode_end = ']';
 
+    private static string $css_class_name = 'highlight';
+
     private static array $casting = [
         'Highlight' => 'Text',
     ];
 
     public function Highlight(): HTMLValue
     {
-        $regex = sprintf('/\%s(.+)%s/', self::getOwner()->config()->get('shortcode_start'), self::getOwner()->config()->get('shortcode_end'));
-        $value = preg_replace($regex, '<span class="is-highlighted">$1</span>', $this->getOwner()->getValue());
+        $regex = sprintf('/\%s(.+)%s/', $this->getOwner()->config()->get('shortcode_start'), $this->getOwner()->config()->get('shortcode_end'));
+        $class = sprintf('<span class="%s">$1</span>', $this->getOwner()->config()->get('css_class_name'));
+
+        $value = preg_replace($regex, $class, $this->getOwner()->getValue());
 
         return HTMLValue::create($value);
     }
